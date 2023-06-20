66bc2b7d6c08c1d6fd89549a31abcfbaaef08068
androbene <androbene@ua.fm>
2023-06-20 19:37:30 +0300

    TRY use JsonSerializable

--- lib/models/fav_repo.dart
+++ lib/models/fav_repo.dart
@@ -1,10 +1,21 @@
 import 'package:hive/hive.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'fav_repo.g.dart';
+
+@JsonSerializable()
 class FavRepo {
+  @JsonKey(name: 'html_url')
   String url;
+  @JsonKey(name: 'name')
   String name;
 
   FavRepo(this.url, this.name);
+
+  factory FavRepo.fromJson(Map<String, dynamic> json) =>
+      _$FavRepoFromJson(json);
+
+  Map<String, dynamic> toJson(n) => _$FavRepoToJson(this);
 
   @override
   String toString() => '[$name, $url]'; // Just for debug
---
+++ lib/models/fav_repo.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fav_repo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+FavRepo _$FavRepoFromJson(Map<String, dynamic> json) => FavRepo(
+      json['html_url'] as String,
+      json['name'] as String,
+    );
+
+Map<String, dynamic> _$FavRepoToJson(FavRepo instance) => <String, dynamic>{
+      'html_url': instance.url,
+      'name': instance.name,
+    };
